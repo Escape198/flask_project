3b74630837b60bc0e5c9e8095a899995d7542e42
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-08 16:59:35 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from app import db, session, Base
 
 
-class Video(Base):
-    __tablename__ = 'videos'
+class Message(Base):
+    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
+    text = db.Column(db.String(250), nullable=False)
+    success = db.Column(db.Integer, primary_key=True)
+
