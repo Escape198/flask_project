c16be69e3eecdc0dbf72fdbffe6fbd9c71720385
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-08 22:44:29 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,9 +1,41 @@
 from app import db, session, Base
+from sqlalchemy.orm import relationship
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from passlib.hash import bcrypt
 
 
 class Message(Base):
     __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     messages = db.Column(db.String(250), nullable=False)
     status = db.Column(db.String(250), nullable=False, default='review')
     success = db.Column(db.String(250), nullable=False, default='False')
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    messages = relationship('Message', backref='user', lazy=True)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.email = kwargs.get('email')
+        self.password = bcrypt.hash(kwargs.get('password'))
+
+    def get_token(self, expire_time=24):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(
+            identity=self.id, expires_delta=expire_delta)
+        return token
+
+    @classmethod
+    def authenticate(cls, email, password):
+        user = cls.query.filter(cls.email == email).one()
+        if not bcrypt.verify(password, user.password):
+            raise Exception('No user with this password')
+        return user
