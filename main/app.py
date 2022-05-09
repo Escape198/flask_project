36bf3af19662f9795b0eef1e048412b148ed6692
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-09 03:56:24 +0300

    Update app.py

--- main/app.py
+++ main/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+from flask_apispec.extension import FlaskApiSpec
+from flask_apispec import use_kwargs, marshal_with
+from flask import render_template
+
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec import APISpec
 
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
-
 from config import Config
+
+import logging
+
+from schemas import MessageSchema, UserSchema, AuthSchema
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -24,102 +34,117 @@ Base.query = session.query_property()
 
 jwt = JWTManager(app)
 
+
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='videoblog',
+        version='v1',
+        openapi_version='2.0',
+        plugins=[MarshmallowPlugin()],
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/'
+})
+
 from models import *
 
 Base.metadata.create_all(bind=engine)
 
 
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-@app.route('/message', methods=['GET'])
+    formatter = logging.Formatter(
+        '%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    file_handler = logging.FileHandler('main/log/api.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+logger = setup_logger()
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/api/v1/message', methods=['GET'])
 @jwt_required()
+@marshal_with(MessageSchema(many=True))
 def get_list():
-    user_id = get_jwt_identity()
-    messages = Message.query.filter(Message.user_id == user_id).all()
-    serialized = []
-    for message in messages:
-        serialized.append({
-            'id': message.id,
-            'messages': message.messages,
-            'status': message.status,
-            'success': message.success
-        })
-    return jsonify(serialized)
+    try:
+        user_id = get_jwt_identity()
+        messages = Message.query.filter(Message.user_id == user_id).all()
+    except Exception as e:
+        logger.warning(
+            f'user:{user_id} message - read action failed with errors: {e}')
+        return {'message': str(e)}, 400
+    return messages
 
 
-@app.route('/message_confirmation', methods=['POST'])
+@app.route('/api/v1/message', methods=['POST'])
 @jwt_required()
-def update_list():
-    user_id = get_jwt_identity()
-    new_one = Message(user_id=user_id, **request.json)
-
-    session.add(new_one)
-    session.commit()
-    serialized = {
-        'id': new_one.id,
-        'messages': new_one.messages,
-    }
-    return jsonify(serialized)
-
-'''
-@app.route('/message/<int:message_id>', methods=['PUT'])
-@jwt_required()
-def update_message(message_id):
-    user_id = get_jwt_identity()
-    item = Message.query.filter(
-        Message.id == message_id,
-        Message.user_id == user_id
-    ).first()
-    params = request.json
-
-    if not item:
-        return {'message': 'No message with this id'}, 400
-    for key, value in params.items():
-        setattr(item, key, value)
-    session.commit()
-    serialized = {
-        'id': message.id,
-        'messages': message.messages,
-        'status': message.status,
-        'success': message.success
-    }
-    return serialized
+@use_kwargs(MessageSchema)
+@marshal_with(MessageSchema)
+def update_list(**kwargs):
+    try:
+        user_id = get_jwt_identity()
+        new_one = Message(user_id=user_id, **kwargs)
+        new_one.save()
+    except Exception as e:
+        logger.warning(
+            f'user:{user_id} message - create action failed with errors: {e}')
+        return {'message': str(e)}, 400
+    return new_one
 
 
-@app.route('/message/<int:message_id>', methods=['DELETE'])
-@jwt_required()
-def delete_message(message_id):
-    user_id = get_jwt_identity()
-    item = Message.query.filter(
-        Message.id == message_id,
-        Message.user_id == user_id).first()
-    if not item:
-        return {'message': 'No message with this id'}, 400
-    session.delete(item)
-    session.commit()
-    return '', 204
-'''
-
-@app.route('/register', methods=['POST'])
-def register():
-    params = request.json
-    user = User(**params)
-    session.add(user)
-    session.commit()
-    token = user.get_token()
-    return {'access_token': token}
+@app.route('/sign_up', methods=['POST'])
+@use_kwargs(UserSchema)
+@marshal_with(AuthSchema)
+def register(**kwargs):
+    try:
+        user = User(**kwargs)
+        session.add(user)
+        session.commit()
+        token = user.get_token()
+    except Exception as e:
+        logger.warning(
+            f'registration failed with errors: {e}')
+        return {'message': str(e)}, 400
+    return {'success': True}
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    params = request.json
-    user = User.authenticate(**params)
-    token = user.get_token()
-    return {'access_token': token}
+@app.route('/auth', methods=['POST'])
+@use_kwargs(UserSchema(only=('email', 'password')))
+@marshal_with(AuthSchema)
+def login(**kwargs):
+    try:
+        user = User.authenticate(**kwargs)
+        token = user.get_token()
+    except Exception as e:
+        logger.warning(
+            f'login with email {kwargs["email"]} failed with errors: {e}')
+        return {'message': str(e)}, 400
+    return {'success': True, 'access_token': token}
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     session.remove()
+
+
+@app.errorhandler(422)
+def handle_error(err):
+    headers = err.data.get('headers', None)
+    messages = err.data.get('messages', ['Invalid Request.'])
+    logger.warning(f'Invalid input params: {messages}')
+    if headers:
+        return jsonify({'message': messages}), 400, headers
+    else:
+        return jsonify({'message': messages}), 400
 
 
 if __name__ == '__main__':
