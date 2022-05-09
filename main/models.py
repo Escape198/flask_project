74968789d0c7387539d5ccfef96b4e78048499d7
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-09 03:56:01 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -6,20 +6,70 @@ from passlib.hash import bcrypt
 
 
 class Message(Base):
-    __tablename__ = 'messages'
+    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    messages = db.Column(db.String(250), nullable=False)
-    status = db.Column(db.String(250), nullable=False, default='review')
-    success = db.Column(db.String(250), nullable=False, default='False')
+    message = db.Column(db.String(500), nullable=False)
+    status = db.Column(db.String(10), nullable=False, default='review')
+    success = db.Column(db.String(10), nullable=False, default='False')
+
+    @classmethod
+    def get_user_list(cls, user_id):
+        try:
+            messages = cls.query.filter(cls.user_id == user_id).all()
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+        return messages
+
+    def save(self):
+        try:
+            session.add(self)
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+
+    @classmethod
+    def get(cls, message_id, user_id):
+        try:
+            video = cls.query.filter(
+                cls.id == message_id,
+                cls.user_id == user_id
+            ).first()
+            if not video:
+                raise Exception('No messages with this id')
+        except Exception:
+            session.rollback()
+            raise
+        return video
+
+    def update(self, **kwargs):
+        try:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+
+    def delete(self):
+        try:
+            session.delete(self)
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+
 
 
 class User(Base):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
     messages = relationship('Message', backref='user', lazy=True)
 
     def __init__(self, **kwargs):
