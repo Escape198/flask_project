b3bb0d0def0038863245b246c7c57d59187cf98b
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-09 03:57:09 +0300

    Update tests.py

--- main/tests.py
+++ main/tests.py
@@ -2,33 +2,22 @@ from app import client
 from models import Message
 
 
+token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY1MjA0NzQ5NSwianRpIjoiZjE0ZWNhN2EtNDc2ZS00MmY3LTkxMDMtMTEwMDBjNDNiZWQ2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjUyMDQ3NDk1LCJleHAiOjE2NTQxMjEwOTV9.PPdjJMUoem2XtcqjH-ZrXc6229PYAaoYoqwA8aMj00E'
+headers = {'Authorization': f'Bearer {token}',
+'Content-Type': 'application/json'}
+
+
 def test_post():
     data = {
-        'messages': 'Unit Tests',
-    }
+        'messages': 'Unit Tests'
+        }
 
-    res = client.post('/message', json=data)
+    res = client.post('api/v1/message', json=data, headers=headers)
 
     assert res.status_code == 200
 
 def test_get():
-    res = client.get('/message')
+    res = client.get('api/v1/message', headers=headers)
 
     assert res.status_code == 200
-
     assert len(res.get_json()) == len(Message.query.all())
-    assert res.get_json()[0]['id'] == 1
-'''
-def test_put():
-    res = client.put('/message/1', json={'message': 'UPD'})
-
-    assert res.status_code == 200
-    assert Message.query.get(1).message == 'UPD'
-
-
-def test_delete():
-    res = client.delete('/message/1')
-
-    assert res.status_code == 204
-    assert Message.query.get(1) is None
-'''
