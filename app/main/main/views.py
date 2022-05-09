1773b531f918066b204990f7c60342ed7c92236c
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-09 22:13:10 +0300

    Update views.py

--- app/main/main/views.py
+++ app/main/main/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_apispec import use_kwargs, marshal_with
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from main import logger, docs
+from main import logger
 from main.schemas import MessageSchema
 from main.base_view import BaseView
 from main.models import Message
@@ -48,6 +48,8 @@ def get_list():
 def update_list(**kwargs):
     try:
         user_id = get_jwt_identity()
+        kwargs['success'] = 'True'
+        kwargs['status'] = 'blocked' if kwargs['message'].lower() == 'абракадабра' else 'correct'
         new_one = Message(user_id=user_id, **kwargs)
         new_one.save()
     except Exception as e:
@@ -66,8 +68,3 @@ def handle_error(err):
         return jsonify({'message': messages}), 400, headers
     else:
         return jsonify({'message': messages}), 400
-
-
-docs.register(get_list, blueprint='messages')
-docs.register(update_list, blueprint='messages')
-ListView.register(messages, docs, '/main', 'listview')
